443aea57f3eb70a608683d80fb33a719a3a9841f
luoli0706 <143058679+luoli0706@users.noreply.github.com>
2025-06-15 18:13:30 +0800

    Add the arco Design style

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import '@arco-design/web-react/dist/css/arco.css';
+
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen.ts';
