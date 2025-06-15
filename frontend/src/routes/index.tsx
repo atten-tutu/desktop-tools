b2bedb42428e0b458cde3c9cb2663358c8e5ed4c
luoli0706 <143058679+luoli0706@users.noreply.github.com>
2025-06-15 17:47:05 +0800

    Update the theme plugin

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { Link } from '@tanstack/react-router'; // 引入 Link 组件
 
 export const Route = createFileRoute('/')({
   component: App,
-})
+});
 
 export default function App() {
-
   return (
     <>
       <h1>INDEX</h1>
+      <Link to="/theme">切换主题</Link> {/* 添加导航链接 */}
     </>
-  )
+  );
 }
